a316ad9eedd5d717b7ddfd7e8d6bd273df7e0e42
Alexey <lmokhareva@gmail.com>
2022-11-16 12:01:39 +0300

    Add models

---
+++ Models/Event.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace EventApp.Models
+{
+    public class Event
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public string Speaker { get; set; }
+        [Required]
+        public string Place { get; set; }
+        [DisplayName("Date")]
+        public DateTime CreatedDate { get; set; }
+    }
+}
