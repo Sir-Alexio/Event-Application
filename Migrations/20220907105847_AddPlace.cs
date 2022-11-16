18c5deae81d981c4503aac7dff83adcaf9b77c6d
Alexey <lmokhareva@gmail.com>
2022-11-16 12:03:13 +0300

    Add Migrations

---
+++ Migrations/20220907105847_AddPlace.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EventApp.Migrations
+{
+    public partial class AddPlace : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Place",
+                table: "Events",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Place",
+                table: "Events");
+        }
+    }
+}
---
+++ Migrations/20220927064141_Add db.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EventApp.Migrations
+{
+    public partial class Adddb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
